8878d7ead2f5452e81540922e650725334098b98
ex-Plant <konradantonik@gmail.com>
2025-11-06 16:31:30 +0100

    add perttier

--- components/form.tsx
+++ components/form.tsx
@@ -1,3 +1,31 @@
+import { Controller } from "react-hook-form";
+import { Field, FieldContent, FieldDescription, FieldError, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import React from "react";
+
 export function FormInput() {
-  return null
+  return <Controller
+    // we start by adding control - after that we are getting type safety
+    // control={control}
+    name={`company_name`}
+    // render function  gives us state of the field
+    render={({ field, fieldState }) => {
+      return (
+        // if data is invalid it will change the color to red
+        <Field data-invalid={fieldState.invalid}>
+          {/*Field content makes children insight a bit tighter*/}
+          <FieldContent>
+            <FieldDescription>Opis pola </FieldDescription>
+            {/* If we connect htmlFor and id, clicking on the label
+             will highlight the input for us*/}
+            <FieldLabel htmlFor={field.name}>nip</FieldLabel>
+          </FieldContent>
+          {/* aria-invalid gives us styling of the whole field
+           like red highlight if field is invalid */}
+          <Input {...field} id={field.name} aria-invalid={fieldState.invalid} />
+          {fieldState.error && <FieldError errors={[{ message: 'Error message' }]} />}
+        </Field>
+      );
+    }}
+  />
 }
